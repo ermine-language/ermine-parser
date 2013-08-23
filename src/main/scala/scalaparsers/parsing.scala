290f2c582a043f85abac973754025e9ad6006fa9
pipocaQuemada <eliyahu.ben.miney@gmail.com>
2013-08-23 14:20:39 -0400

    added most of ermine.scala to parsing.scala

--- src/main/scala/scalaparsers/parsing.scala
+++ src/main/scala/scalaparsers/parsing.scala
@@ -9,11 +9,13 @@ import parsing.Diagnostic._
 import java.lang.Character._
 import Ordering._
 
-import com.clarifi.reporting.util.YMDTriple.ymdPivotTimeZone
+import parsing.{ Relocatable, Located, Loc }
+import scalaz.{ Lens }
+import scala.collection.immutable.List
+import Lens._
+import java.util.TimeZone
 
 package object parsing {
-  def dist[A](xs: List[Localized[A]]): Localized[List[A]] = Localized(xs.map(_.extract), xs.flatMap(_.names), xs.traverse_[Parser](_.unbind))
-
   def unit[A](a: A): Parser[A] = new Parser[A] {
     def apply(s: ParseState, vs: Supply) = suspend(Return(Pure(a)))
     override def map[B](f: A => B) = unit(f(a))
@@ -303,7 +305,7 @@ package object parsing {
       d <- nat_.filter(1 to 31 contains)
     } yield {
       import java.util.Calendar
-      val c = Calendar getInstance ymdPivotTimeZone
+      val c = Calendar getInstance (TimeZone getTimeZone "GMT")
       c set (Calendar.MILLISECOND, 0)
       c set (y.toInt, m.toInt - 1, d.toInt, 0, 0, 0)
       c.getTime
@@ -363,8 +365,52 @@ package object parsing {
   def prec: Parser[Int] = nat.filter(_ <= 10L).map(_.toInt) scope "precedence between 0 and 10"
   def underscore: Parser[Unit] = token((ch('_') >> notFollowedBy(tailChar)) attempt "underscore")
 
-  // kinds
-  def prefix (n: String, p: Int = 10) = Local(n, Prefix(p))
-  def postfix(n: String, p: Int = 10) = Local(n, Postfix(p))
-  def infix  (n: String, p: Int = 9, a: Assoc = AssocL) = Local(n, Infix(p,a))
+
+  def setLoc[T<:Located](t: T, l: Loc)(implicit T:Relocatable[T]) = T.setLoc(t,l)
+  def location[T <: Located](implicit T:Relocatable[T]) = lensu[T,Loc]((s, u) => T.setLoc(s, u), _.loc)
+
+  def skip[T[+_]](p:Functorial[T,Any]): T[Unit] = p as ()
+  def as[T[+_],A](p:Functorial[T,Any], a: A): T[A] = p as a
+  def filterMap[T[+_],A,B](p: Filtered[T,A])(f: A => Option[B]): T[B] = p filterMap f
+  def many1[T[+_],A](p: Alternating[T,A]): T[List[A]] = p some // can't use some!
+  def skipSome[T[+_]](p:Alternating[T,Any]): T[Unit] = p skipSome
+  def many[T[+_],A](p: Alternating[T,A]): T[List[A]] = p many
+  def skipMany[T[+_]](p:Alternating[T,Any]): T[Unit] = p skipMany
+  def manyTill[T[+_],A](p: Alternating[T,A], end: T[Any]) = p manyTill end
+  def skipManyTill[T[+_],A](p: Alternating[T,A], end: T[Any]) = p skipManyTill end
+  def sepEndBy[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepEndBy sep
+  def skipSepEndBy[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p skipSepEndBy sep
+  def sepEndBy1[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepEndBy1 sep
+  def skipSepEndBy1[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p skipSepEndBy1 sep
+  def sepBy[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepBy sep
+  def sepBy1[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepBy1 sep
+  def skipSepBy[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepBy sep
+  def skipSepBy1[T[+_],A](p: Alternating[T,A], sep: T[Any]) = p sepBy1 sep
+  def chainr[T[+_],A](p: Alternating[T,A])(op: T[(A,A) => A]) = p chainr op
+  def chainl[T[+_],A](p: Alternating[T,A])(op: T[(A,A) => A]) = p chainl op
+  def endBy[T[+_],A](p: Alternating[T,A], end: T[Any]) = p endBy end
+  def endBy1[T[+_],A](p: Alternating[T,A], end: T[Any]) = p endBy1 end
+  def skipEndBy[T[+_],A](p: Alternating[T,A], end: T[Any]) = p skipEndBy end
+  def skipEndBy1[T[+_],A](p: Alternating[T,A], end: T[Any]) = p skipEndBy1 end
+  def extract[T[+_],A](w: Comonadic[T,A]): A = w.extract
+  def extend[T[+_],A,B](w: Comonadic[T,A])(f: T[A] => B): T[B] = w.extend[B](f)
+  def scope[T[+_],A](w: Scoped[T,A], desc: String): T[A] = w scope desc
+
+  def mapAccum_[S,A](g: S, xs: List[A])(f: (S, A) => S): S = xs match {
+    case List()  => g
+    case a :: as => mapAccum_(f(g,a),as)(f)
+  }
+
+  def mapAccum[S,A,B](g: S, xs: List[A])(f: (S, A) => (S, B)): (S, List[B]) = xs match {
+    case List() => (g, List())
+    case a :: as =>
+      val (gp,b) = f(g,a)
+      val (gpp,bs) = mapAccum(gp, as)(f)
+      (gpp, b :: bs)
+  }
+
+  def die(d: Document) = throw Death(d)
+  // def empty = throw SubstException(None)
 }
+
+// vim: set ts=4 sw=4 et:
